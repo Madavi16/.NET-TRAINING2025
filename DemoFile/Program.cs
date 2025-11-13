6cbd5c66d8bbe5f55b21972d423aab6993305989
Madavi Ravi <madavir@infinite.com>
2025-11-13 10:28:17 +0530

    Revert "Delete DemoFile directory"
    This reverts commit 6380d4f4a8452e653da75cdaa5172734b95805b5.

---
+++ DemoFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DemoFile
+{
+    internal class SampleProgram
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 10;
+            float avg_score = 67.45f;
+            double precise_value = 12345.674920479282;
+            decimal money_amount = 1234567890.12m;
+            char grade = 'A';
+            string message = "Hello, welcome to the session";
+
+            Console.WriteLine("Num1 value is : "+num1);
+            Console.WriteLine("Num2 value is : {0} ", num2);
+
+            Console.WriteLine($"Average Score = {avg_score} \n Precise value = {precise_value} \n Amount = {money_amount}" );
+            Console.Write($"Grade = {grade}\n");
+            Console.WriteLine("Message : "+message);
+            
+            //Console.ReadLine();
+
+        }
+
+    }
+}
