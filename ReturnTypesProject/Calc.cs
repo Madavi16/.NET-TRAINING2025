889352e97499f44ebc2649c7dfac2f40d58637ea
Madavi Ravi <madavir@infinite.com>
2025-11-13 10:26:53 +0530

    Revert "Delete ReturnTypesProject directory"
    This reverts commit 1749934aa7705972c6e25e566268c7afc5a2fd5c.

---
+++ ReturnTypesProject/Calc.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnTypesProject
+{
+    public struct CalcResultStruct
+    {
+        public double Sum;
+        public double Difference;
+        public double Product;
+        public double Division;
+    }
+
+    public class CalcResultClass
+    {
+        public double Sum;
+        public double Difference;
+        public double Product;
+        public double Division;
+    }
+
+    internal class Calc
+    {
+        //Tuple
+        //public (double Sum, double Difference, double Product, double Division) Calculate(double a, double b)
+        //{
+        //    double sum = a + b;
+        //    double diff = a - b;
+        //    double prod = a * b;
+        //    double div = (b != 0) ? a / b : double.NaN;
+
+        //    return (sum, diff, prod, div);
+
+        //}
+
+        //Out keyword
+        //public void Calculate(double a, double b,out double sum,out double diff,out double prod, out double div)
+        //{
+        //     sum = a + b;
+        //     diff = a - b;
+        //     prod = a * b;
+        //     div = (b != 0) ? a / b : double.NaN;
+        //}
+
+
+        //using struct
+       //public CalcResultStruct CalculateUsingStruct(double a, double b)
+       // {
+       //     CalcResultStruct result;
+       //     result.Sum = a + b;
+       //     result.Difference = a - b;
+       //     result.Product = a * b;
+       //     result.Division = (b != 0) ? a / b : double.NaN;
+
+       //     return result;
+
+       // }
+
+       // //using class
+       // public CalcResultClass CalculateUsingClass(double a, double b)
+       // {
+       //     CalcResultClass result= new CalcResultClass();
+       //     result.Sum = a + b;
+       //     result.Difference = a - b;
+       //     result.Product = a * b;
+       //     result.Division = (b!=0)?a/b: double.NaN;
+
+       //     return result;
+
+       // }
+
+        //using list
+        public struct CalcResult 
+        {
+            public double A;
+            public double B;
+
+
+        }
+
+        public CalcResult CalculateUsingClass(double a, double b)
+        {
+            CalcResult result = new CalcResultClass();
+            result.Sum = a + b;
+            result.Difference = a - b;
+            result.Product = a * b;
+            result.Division = (b != 0) ? a / b : double.NaN;
+
+            return result;
+
+        }
+        public void DisplayAll(List<CalcResult> results)
+        {
+            Console.WriteLine("\n=== Calculation Details ===");
+            foreach(var r in results)
+            {
+                Console.WriteLine($"Sum = {r.Sum}");
+            }
+
+        }
+    }
+    
+}
---
+++ ReturnTypesProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnTypesProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc calc=new Calc();
+
+            Console.WriteLine("Enter the first num: ");
+            double x=Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second num: ");
+            double y=Convert.ToDouble(Console.ReadLine());
+
+            //using tuple
+            //var result=calc.Calculate(x, y);
+            //Console.WriteLine($"\nSum= {result.Sum}");
+            //Console.WriteLine($"\nDifference = {result.Difference}");
+            //Console.WriteLine($"\nMultiplication = {result.Product}");
+            //Console.WriteLine($"\nDivision = {result.Division}");
+
+            //using out keyword
+            //calc.Calculate(x, y, out double sum, out double diff, out double prod, out double div);
+            //Console.WriteLine($"\nSum: {sum}\nDiff: {diff}\nProduct: {prod}\nDivision:{div}");
+
+
+            //using struct
+            var structResult = calc.CalculateUsingStruct(x, y);
+            Console.WriteLine($"\nUsing Struct: ");
+            Console.WriteLine($"Sum: {structResult.Sum}");
+            Console.WriteLine($"Difference: {structResult.Difference}");
+            Console.WriteLine($"Product: {structResult.Product}");
+            Console.WriteLine($"Division: {structResult.Division}");
+
+
+            //using class
+            var classResult=calc.CalculateUsingClass(x, y);
+            Console.WriteLine($"\nUsing class: ");
+            Console.WriteLine($"Sum= {classResult.Sum}");
+            Console.WriteLine($"Difference= {classResult.Difference}");
+            Console.WriteLine($"Product= {classResult.Product}");
+            Console.WriteLine($"Division= {classResult.Division}");
+
+            Console.ReadLine();
+        }
+    }
+}
