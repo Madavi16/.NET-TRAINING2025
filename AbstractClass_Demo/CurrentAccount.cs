1cd1af23296fbbecf4924f734421c09f3d55d212
Madavi Ravi <madavir@infinite.com>
2025-11-13 10:30:00 +0530

    Revert "Delete AbstractClass_Demo directory"
    This reverts commit 3889cad95b46def766ff32439ab5c8db2ee1df19.

---
+++ AbstractClass_Demo/CurrentAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass_Demo
+{
+   public class CurrentAccount:BankAccount
+    {
+        public CurrentAccount(string accountNumber, double initialBalance ) : base(accountNumber, initialBalance)
+        {
+            //this.AccountNumber= accountNumber;
+            //this.initialBalance = initialBalance;
+        }
+
+        public override void CalculateInterest()
+        {
+            Console.WriteLine("\nCurrent accounts do not earn interest.");
+        }
+
+
+    }
+}
---
+++ AbstractClass_Demo/SavingsAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass_Demo
+{
+    public class SavingsAccount :BankAccount
+    {
+        public SavingsAccount(string accountNumber, double initialBalance) : base(accountNumber, initialBalance)
+        {
+            this.Balance = initialBalance;
+            this.AccountNumber = accountNumber;
+        }
+        public override void CalculateInterest()
+        {
+            double interest = Balance * 0.05;
+            Balance += interest;
+            Console.WriteLine($"Interest of {interest} added. New Balance is {Balance}");
+           
+        }
+    }
+}
