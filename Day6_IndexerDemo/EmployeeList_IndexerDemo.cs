91c450cd690197810122afb094b2eb1bcf577c8b
Madavi Ravi <madavir@infinite.com>
2025-11-13 10:28:48 +0530

    Revert "Delete Day6_IndexerDemo directory"
    This reverts commit 3ee84fbae804dea22ac0d3145fda1bce6efa0378.

---
+++ Day6_IndexerDemo/EmployeeList_IndexerDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_IndexerDemo
+{
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Gender { get; set; }
+    }
+    internal class EmployeeList_IndexerDemo
+    {
+       
+            List<Employee> employees = new List<Employee>()
+            { new Employee() { EmployeeId = 1, EmployeeName = "Jana", Gender = "Male" },
+                new Employee() { EmployeeId = 2, EmployeeName = "Mahe", Gender = "Female" },
+                new Employee() { EmployeeId = 3 , EmployeeName = "Chiru", Gender = "Male" }
+            };
+
+
+        public string this[int empid]
+        {
+            get
+            {
+                return employees.FirstOrDefault(e => e.EmployeeId == empid)?.EmployeeName;
+
+            }
+
+            set
+            {
+                employees.FirstOrDefault(e => e.EmployeeId == empid).EmployeeName = value;
+            }
+        }
+        public string this[String name]
+        {
+            get
+            {
+                return employees.FirstOrDefault(e => e.EmployeeName==name)?.Gender;
+
+            }
+            set
+            {
+                employees.FirstOrDefault(e => e.EmployeeName==name).Gender = value;
+            }
+        }
+    }
+}
