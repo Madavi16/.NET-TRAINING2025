55942f0565aac20dddea8a3c5f5a9edab108893e
Madavi Ravi <madavir@infinite.com>
2025-11-13 10:27:43 +0530

    Revert "Delete FunctionOrMethod_Overriding directory"
    This reverts commit 4a9a173e44636a20b57e05b7f045cf8a69550dd3.

---
+++ FunctionOrMethod_Overriding/CreditCardPayment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionOrMethod_Overriding
+{
+    internal class CreditCardPayment:PaymentMethod
+    {
+        public override string Provider => "Credit Card Provider";
+
+        public override bool ProcessPayment(decimal amount)
+        {
+            base.ProcessPayment(amount);
+            if(amount>0 && amount<=5000)
+            {
+                Console.WriteLine($"Processing credit card payment of amount: {amount}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Processing payment with credit card is failed.");
+                return false;
+            }
+        }
+    }
+}
